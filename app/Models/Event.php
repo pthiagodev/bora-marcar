b013e44ff231df8d3931c9945516e0a4c66becb2
Thiago de Assis <pthiagodev@gmail.com>
2023-06-06 22:26:17 -0300

    adiciona métodos dos relacionamentos

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Event extends Model
 {
@@ -12,4 +13,15 @@ class Event extends Model
     protected $fillable = [
         'name'
     ];
+
+    public function location(): BelongsTo
+    {
+        return $this->belongsTo(Location::class);
+    }
+
+    public function organizer(): BelongsTo
+    {
+        return $this->belongsTo(Organizer::class);
+    }
+
 }
