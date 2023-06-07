743eca9ad115f2dd65694e4e8accdb0701f23a18
Thiago de Assis <pthiagodev@gmail.com>
2023-06-07 00:10:41 -0300

    cria nav-bar para navegação entre as views

---
+++ resources/views/components/nav-bar.blade.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Bora Marcar?</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Painel</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('events.index') }}">Eventos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('locations.index') }}">Locais</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('organizers.index') }}">Organizadores</a>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Busque um evento" aria-label="Buscar">
+                <button class="btn btn-outline-success" type="submit">Buscar</button>
+            </form>
+        </div>
+    </div>
+</nav>
