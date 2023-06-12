50b56dcec0a27f9680738861c9ab7ef64dda0ece
Thiago de Assis <pthiagodev@gmail.com>
2023-06-12 19:14:30 -0300

    registra os repositórios no Provider

--- app/Providers/RepositoryProvider.php
+++ app/Providers/RepositoryProvider.php
@@ -4,12 +4,18 @@ namespace App\Providers;
 
 use App\Repositories\Event\EloquentEventsRepository;
 use App\Repositories\Event\EventsRepository;
+use App\Repositories\Location\EloquentLocationsRepository;
+use App\Repositories\Location\LocationsRepository;
+use App\Repositories\Organizer\EloquentOrganizersRepository;
+use App\Repositories\Organizer\OrganizersRepository;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryProvider extends ServiceProvider
 {
     public array $bindings = [
-        EventsRepository::class => EloquentEventsRepository::class
+        EventsRepository::class => EloquentEventsRepository::class,
+        LocationsRepository::class => EloquentLocationsRepository::class,
+        OrganizersRepository::class => EloquentOrganizersRepository::class
     ];
 
 }
