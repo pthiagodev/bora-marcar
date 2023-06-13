8a6b869b73cd4aa707345fea131957dcf2f6b4b6
Thiago de Assis <pthiagodev@gmail.com>
2023-06-12 21:40:48 -0300

    ajusta os imports das rotas

--- routes/web.php
+++ routes/web.php
@@ -4,26 +4,30 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\EventsController;
 use App\Http\Controllers\LocationsController;
 use App\Http\Controllers\OrganizersController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('/events', EventsController::class)
-    ->except('show');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-Route::resource('/locations', LocationsController::class)
-    ->except('show');
+    Route::resource('/events', EventsController::class)
+        ->except('show');
 
-Route::resource('/organizers', OrganizersController::class)
-    ->except('show');
+    Route::resource('/locations', LocationsController::class)
+        ->except('show');
+
+    Route::resource('/organizers', OrganizersController::class)
+        ->except('show');
+
+});
+
+require __DIR__.'/auth.php';
