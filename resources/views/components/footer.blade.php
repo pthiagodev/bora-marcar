e5452062772dc2f32289680798809d12d75a0faa
Thiago de Assis <pthiagodev@gmail.com>
2023-06-13 11:35:22 -0300

    cria componente de rodapé

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<div class="shadow-sm">
+    <footer class="font-semibold text-center text-gray-500">
+        <p class="py-3">2023 <i class="bi bi-c-circle"></i> Desenvolvido por Thiago de Assis  com Laravel 10 | Projeto fictício sem fins comerciais.</p>
+    </footer>
+</div>
